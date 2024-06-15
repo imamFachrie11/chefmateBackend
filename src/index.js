24a1bebba3d58ebecca0fbbbfd1114eca54496ae
SadamMahendra <sadam.mahendra@gmail.com>
2024-06-15 14:41:01 +0700

    menambahkan src

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(process.env.SERVER_PORT || 3000, () => {
+  console.log("Server Running");
+});
