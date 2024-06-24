962a9678375c9b8c2296d5506ea12061533a71e6
CandraEriansyah <candraeriansyah@gmail.com>
2024-06-24 22:23:13 +0700

    create controllers & routes file creat_recipe

--- routes/recipe.route.js
+++ routes/recipe.route.js
@@ -2,10 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
+const {
+  createRecipe,
+  getRecipes,
+} = require("../controllers/creat_recipe.controllers");
+
 const { validateToken } = require("../middlewares/auth");
 const { index, update } = require("../controllers/recipe.controllers");
 
 router.get("/:id_recipe?", index);
 router.patch("/:id_recipe?", validateToken, update);
+
+router.post("/recipes", createRecipe);
+router.get("/recipes", getRecipes);
 
 module.exports = router;
