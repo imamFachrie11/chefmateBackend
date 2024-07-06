7f8353f5819512736bf3d3d693a8e137772e315b
alfahmist <gustialfahmi@gmail.com>
2024-07-06 22:34:00 +0700

    hapus created_at create komentar karena sudha otomatis

--- controllers/komentar.controller.js
+++ controllers/komentar.controller.js
@@ -1,17 +1,13 @@
-// const { Op } = require ('sequelize');
-
 const { komentar: komentarModel } = require("../models");
 
 const createKomentar = async (req, res, next) => {
   try {
     const { id_user, id_recipe, deskripsi } = req.body;
-    const create_at = new Date();
 
     await komentarModel.create({
       deskripsi: deskripsi,
       id_user: id_user,
       id_recipe: id_recipe,
-      create_at: create_at,
     });
 
     return res.status(200).send({
